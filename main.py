0d97d5256d31afbacea40340189e03f7baaa6d40
zilogfa <Zilogfa@live.com>
2023-12-14 15:14:31 -0800

    UI updated.

--- main.py
+++ main.py
@@ -17,44 +17,37 @@ import openai
 
 # Flask & OpenAPI Config Config
 app = Flask(__name__)
-API_KEY = open('API_KEY.txt', 'r').read()
+API_KEY = open('API_KEY_.txt', 'r').read()
 openai.api_key = API_KEY
 
+app.secret_key = 'secret_key'
 chat_log = []
 
+
 # Home Page
-
-
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
-
+    session.setdefault('chat_log', [])  # Initialize chat log in session
     return render_template('base.html')
 
 
 @app.route('/post_data', methods=['POST'])
 def post_data():
     user_input = request.json
-    print(user_input)
-    chat_log.append({"role": "user", "content": user_input})
-    print("user data appended to chatlog")
-    print(chat_log)
+    session['chat_log'].append(f"user: {user_input}")
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=chat_log
+            messages=session['chat_log']
         )
-        print('msg sent to ChatGPT')
-        assistant_response = response['choices'][0]['message']['content']
-        print('response from Chat GPT')
-        gpt = assistant_response.strip("\n").strip()
-        print(gpt)
-        chat_log.append(
-            {"role": "assistant", "content": assistant_response.strip("\n").strip()})
-        print(f'*** response appended to chatLog: \n{chat_log}')
-        return gpt
-    except openai.error.RateLimitError:
-        print(f':::::::: except :::::::: {openai.error.RateLimitError}')
-        return "Sorry, we have exceeded our chat limit. Please try again later."
+        assistant_response = response['choices'][0]['message']['content'].strip()
+        session['chat_log'].append(f"assistant: {assistant_response}")
+
+        return assistant_response
+    except openai.error.OpenAIError as e:  # Generic OpenAI API error
+        print(f"Error: {e}")
+        return "Sorry, there was an error processing your request."
 
 
 if __name__ == '__main__':
